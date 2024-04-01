b6d985c982cfdab5f390a68920a47103740138d9
Documenter.jl <documenter@juliadocs.github.io>
2024-04-01 14:34:22 +0000

    build based on d6002a1

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = VKSetCovers","category":"page"},{"location":"#VKSetCovers","page":"Home","title":"VKSetCovers","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for VKSetCovers.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [VKSetCovers]","category":"page"}]
+}
